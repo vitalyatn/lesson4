a4e01541c6ef153c801fc7fee62be3f9a5a820fe
vitalya_tn <stolyarov.vitalij@list.ru>
2019-04-21 19:39:24 +1000

    промежуточная версия

--- main.rb
+++ main.rb
@@ -7,8 +7,12 @@ require_relative "cargo_wagon"
 require_relative "passenger_train"
 require_relative "passenger_wagon"
 
-stations = []
-trains = []
+$stations = []
+$trains = []
+
+def check_station? (station)
+  $stations.any? {|st| st.title == station}
+end
 
 loop do
 puts "Выберите действие, которое вы хотите сделать
@@ -32,7 +36,7 @@ case act
      puts "Введите название станции \n(введите 'стоп' для прекращения ввода)"
      title = gets.chomp
      break if title == "стоп"
-     stations << Station.new(title)
+     $stations << Station.new(title)
     end
 
   when 2
@@ -44,9 +48,9 @@ case act
       puts "Введите номер поезда:"
       number_train = gets.chomp
       if train_type == "п"
-        trains << PassengerTrain.new(number_train)
-      elsif type_train == "г"
-        trains << CargoTrain.new(number_train)
+        $trains << PassengerTrain.new(number_train)
+      elsif train_type == "г"
+        $trains << CargoTrain.new(number_train)
       else
         puts "Такой тип поезда создать невозможно!"
       end
@@ -54,11 +58,26 @@ case act
 
   when 3
     puts "Доступные станции:"
-    stations.each {|st| puts st.title}
-    puts "Выберите начальную станцию:"
-    start_station = gets.chomp
-    check_station?
-
+    $stations.each {|st| puts st.title}
+    puts "Доступные поезда:"
+    $trains.each {|tr| puts tr.number}
+    loop do
+      puts "Выберите начальную станцию (или стоп - чтобы выйти):"
+      start_station = gets.chomp
+      break if start_station == "стоп"
+      if check_station?(start_station)
+        puts "Выберите конечную станцию (или стоп - чтобы выйти):"
+        end_station = gets.chomp
+        break if end_station == "стоп"
+        if check_station?(end_station)
+          puts "Маршрут задан!"
+        else
+          puts "Конечная станция задана некорректно!"
+        end
+      else
+        puts "Начальная станция задана некорректно!"
+      end
+    end
 
   when 4
 
@@ -72,10 +91,8 @@ case act
     puts "Неизвестное действие"
   end
 
-  stations.each {|st| puts st.title}
-  trains.each {|tr| puts "#{tr.number} - #{tr.type}"}
+  #$stations.each {|st| puts st.title}
+  #$trains.each {|tr| puts "#{tr.number} - #{tr.type}"}
 end
 
-def check_station?
-  stations.include?(start_station)
-end
+
