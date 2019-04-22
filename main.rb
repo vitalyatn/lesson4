0e0748721e1ecee7cd5db30b7705b70afe5d8870
vitalya_tn <stolyarov.vitalij@list.ru>
2019-04-22 18:06:47 +1000

    Третья версия

--- main.rb
+++ main.rb
@@ -7,66 +7,49 @@ require_relative "cargo_wagon"
 require_relative "passenger_train"
 require_relative "passenger_wagon"
 
-$stations = []
-$trains = []
-$routes = []
-$train = nil
-$route = nil
-$station = nil
-$wagon = nil
+@stations = []
+@trains = []
+@routes = []
+@train = nil
+@route = nil
+@station = nil
+@wagon = nil
 
-def check_station? (station)
-  $stations.any? {|st| st.title == station}
-end
-
-def check_route? (route)
-  $routes.any? do |rt|
-    rt.start_station.title == route.first && rt.end_station.title == route.last
-  end
-end
-
-def check_wagon? (wagon)
-  $train.wagons.any? do |w|
-    w.id == wagon
-  end
-end
 
 def list_of_trains
   puts "Доступные поезда:"
-  $trains.each {|tr| puts tr.number}
+  i = 0
+  @trains.each do |train|
+    puts "#{(i+1).to_s }: номер: '#{train.number}', тип: #{train.type}"
+    i += 1
+  end
 end
 
 def list_of_stations
   puts "Доступные станции:"
-  $stations.each {|st| puts st.title}
+  i = 0
+  @stations.each do |station|
+    puts "#{(i+1).to_s } - #{station.title}"
+    i += 1
+  end
 end
 
 def list_of_routes
   puts "Доступные маршруты:"
-  $routes.each do |route|
-    print "#{route.start_station.title} - #{route.end_station.title} ПОЛНЫЙ МАРШРУТ:"
+  i = 0
+  @routes.each do |route|
+    print "#{(i+1).to_s} : #{route.start_station.title} - #{route.end_station.title} ПОЛНЫЙ МАРШРУТ:"
     route = route.stations
     route.each {|station| print "#{station.title} - "}
     puts ""
+    i += 1
   end
 end
 
 def choose_of_train
-loop do
-      puts "Выберите номер поезда"
-      train_number = gets.chomp
-      if $trains.any? {|tr| tr.number == train_number}
-        puts "Поезд номер #{train_number} выбран!"
-        index_train = $trains.index {|tr| tr.number == train_number}
-        $train = $trains[index_train]
-        break
-      else
-        puts "ОШИБКА: Поезда с таким номером нет.
-        \rПопробывать еще? (введите 'д' или 'н')"
-        user_answer = gets.chomp
-        break if user_answer == 'н'
-      end
-    end
+  puts "Введите номер строки."
+  str_number = gets.chomp.to_i
+  @train = @trains[str_number-1]
 end
 
 loop do
@@ -93,7 +76,7 @@ case act
      \r(введите 'стоп' для прекращения ввода)"
      title = gets.chomp
      break if title == "стоп"
-     $stations << Station.new(title)
+     @stations << Station.new(title)
     end
 
   when 2
@@ -106,9 +89,9 @@ case act
       puts "Введите номер поезда:"
       number_train = gets.chomp
       if train_type == "п"
-        $trains << PassengerTrain.new(number_train)
+         @trains << PassengerTrain.new(number_train)
       elsif train_type == "г"
-        $trains << CargoTrain.new(number_train)
+        @trains << CargoTrain.new(number_train)
       else
         puts "Такой тип поезда создать невозможно!"
       end
@@ -118,42 +101,22 @@ case act
     list_of_stations
     loop do
       puts "Выберите начальную станцию"
-      start_station = gets.chomp
+      start_station = gets.chomp.to_i
       puts "Выберите конечную станцию"
-      end_station = gets.chomp
-      if check_station?(start_station) && check_station?(end_station)
-        index_start_station = $stations.index do |st|
-         st.title == start_station
+      end_station = gets.chomp.to_i
+      @routes << Route.new(@stations[start_station-1],@stations[end_station-1])
+      puts "Маршрут создан!
+      \rДобавить промежуточные станции? (введите 'д' или 'н' )"
+      user_answer = gets.chomp
+      if user_answer == 'д'
+        loop do
+          puts "введите промежуточную станцию"
+          middle_station = gets.chomp.to_i
+          @routes.last.add_station(@stations[middle_station-1])
+          puts "Добавить еще станцию? (введите 'д' или 'н')"
+          user_answer = gets.chomp
+          break if user_answer == 'н'
         end
-        index_end_station = $stations.index do |st|
-         st.title == end_station
-        end
-        start_station = $stations[index_start_station]
-        end_station = $stations[index_end_station]
-        $routes << Route.new(start_station,end_station)
-        puts "Маршрут создан!
-        \rДобавить промежуточные станции? (введите 'д' или 'н' )"
-         user_answer = gets.chomp
-         if user_answer == 'д'
-          loop do
-            puts "введите промежуточную станцию"
-            middle_station = gets.chomp
-            if check_station?(middle_station)
-              index_middle_station = $stations.index do |st|
-               st.title == middle_station
-              end
-              middle_station = $stations[index_middle_station]
-              $routes.last.add_station(middle_station)
-            else
-              puts "Такой станции не существует"
-            end
-            puts "Добавить еще станцию? (введите 'д' или 'н')"
-            user_answer = gets.chomp
-            break if user_answer == 'н'
-          end
-        end
-      else
-        puts "ОШИБКА: Такой маршрут невозможно создать."
       end
       puts "Создать еще маршрут? (введите 'д' или 'н')"
       user_answer = gets.chomp
@@ -165,40 +128,27 @@ case act
     choose_of_train
     list_of_routes
     loop do
-      puts "Выберите маршрут (напишите в формате 'начало-конец')"
-      route = gets.chomp
-      route = route.split('-')
-      route.each {|r| puts r}
-      if check_route?(route)
-        index_route = $routes.index do |rt|
-          rt.start_station.title == route.first && rt.end_station.title == route.last
-        end
-        $route = $routes[index_route]
-        $train.add_route($route)
-        puts "Маршрут к поезду добавлен!"
-        break
-      else
-        puts "ОШИБКА: Такого маршрута нет
-        \rПопробывать еще? (введите 'д' или 'н')"
-        user_answer = gets.chomp
-        break if user_answer == 'н'
-      end
+      puts "Выберите маршрут"
+      route = gets.chomp.to_i
+      @train.add_route(@routes[route-1])
+      puts "Маршрут к поезду добавлен!"
+      break
     end
 
   when 5
     list_of_trains
     choose_of_train
     # $train содержит выбранный поезд
-    puts $train.class
+    puts "Выбран поезд: #{@train.number}, тип: #{@train.type}"
     loop do
       puts "Укажите номер вагона"
       number_wagon = gets.chomp.to_i
-      if $train.is_a? PassengerTrain
-        $wagon = PassengerWagon.new(number_wagon)
+      if @train.is_a? PassengerTrain
+        @wagon = PassengerWagon.new(number_wagon)
       else
-        $wagon = CargoWagon.new(number_wagon)
+        @wagon = CargoWagon.new(number_wagon)
       end
-      $train.add_wagon($wagon)
+      @train.add_wagon(@wagon)
       puts "Добавить еще вагон? (введите 'д' или 'н')"
       user_answer = gets.chomp
       break if user_answer == 'н'
@@ -208,20 +158,14 @@ case act
   when 6
     list_of_trains
     choose_of_train
-    # $train содержит выбранный поезд
+    puts "Выбран поезд: #{@train.number}, тип: #{@train.type}"
     puts "Выберите вагон, который хотите отцепить"
-    $train.wagons.each {|wagon| print wagon.id.to_s + " "}
+    @train.wagons.each {|wagon| print wagon.id.to_s + " "}
     puts ""
-    wagon = gets.chomp.to_i
-    if check_wagon?(wagon)
-      index_wagon = $train.wagons.index do |w|
-        w.id == wagon
-      end
-      puts index_wagon
-      $wagon = $train.wagons[index_wagon]
-      $train.delete_wagon($wagon)
-      #$train.wagons.each {|w| puts w.id.to_s}
-    end
+    wagon_number = gets.chomp.to_i
+    index_wagon = @train.wagons.index { |wagon| wagon.id == wagon_number}
+    @wagon = @train.wagons[index_wagon]
+    @train.delete_wagon(@wagon)
 
   when 7
     list_of_trains
@@ -230,9 +174,9 @@ case act
     puts "Куда перемещаем? в-вперед, н-назад"
     move = gets.chomp
     if move == "в"
-      $train.forward
+      @train.forward
     elsif move == "н"
-      $train.back
+      @train.back
     else
       puts "Неизвестное направление"
     end
@@ -240,21 +184,13 @@ case act
   when 8
     list_of_stations
     puts "выберите станцию"
-    station = gets.chomp
-    if check_station?(station)
-      index_station = $stations.index do |st|
-        st.title == station
-      end
-      station = $stations[index_station]
-      station.trains.each {|train| puts train.number }
-    end
+    station = gets.chomp.to_i
+    station = @stations[station-1]
+    station.trains.each {|train| puts train.number }
 
   else
     puts "Неизвестное действие"
   end
-
-  #$stations.each {|st| puts st.title}
-  #$trains.each {|tr| puts "#{tr.number} - #{tr.type}"}
 end
 
 
